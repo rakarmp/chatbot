2556e2b15b240ec6c5e4a047f13d624500c2ba26
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-06-07 10:03:03 +0700

    add headbar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ChatGPT from "./src";
+import HeaderBar from "./src/components/headbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <HeaderBar />
       <ChatGPT />
       <StatusBar style="auto" />
     </View>
